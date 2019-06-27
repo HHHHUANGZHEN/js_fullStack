62b2d03bcf23c7f538766068135c49f0d8ae3fa4
HuangZhen7 <HuangZhen_49@163.com>
2019-06-27 17:02:08 +0800

    网易音乐

---
+++ vue/vue-music/src/common/mixin.js
@@ -0,0 +1,29 @@
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export const searchMixin = {
+  data () {
+    return {
+      query: '',
+      refreshDelay: 120
+    }
+  },
+  methods: {
+    onQueryChange (query) {
+      this.query = query.trim()
+    },
+    blurInput () {
+      this.$refs.searchBox.blur()    
+    },
+    addQuery (query) {
+      this.$refs.searchBox.setQuery(query)
+    },
+    saveSearch(song) {
+      console.log(song)
+      this.selectPlaySong(song)
+      this.saveSearchHistory(this.query)
+    },
+    ...mapActions([
+      'saveSearchHistory', 'selectPlaySong'
+    ])
+  },
+}
