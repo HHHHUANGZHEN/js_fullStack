d89bf20b364eddc8ba2cc85431bf8ab8237eaa6c
HuangZhen7 <HuangZhen_49@163.com>
2019-07-05 14:47:42 +0800

    兄弟组件传值provide和inject

---
+++ vue/vue-components/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <v-menu></v-menu>
+    <v-button size="large" @click.native="handleClick">按钮</v-button>
+  </div>
+</template>
+
+<script>
+import button from './com/button'
+import menu from './com/menu'
+export default {
+  name: 'HelloWorld',
+  provide () {
+    return {
+      name: this.obj
+    }
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      obj: {
+        name: '蜗牛'
+      }
+    }
+  },
+  methods: {
+    handleClick (val) {
+      // console.log(val)
+    }
+  },
+  components: {
+    'v-button': button,
+    'v-menu': menu
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/vue-components/src/components/com/menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="menu">
+    {{message}}  {{name.name}} {{app.user}}
+  </div>
+</template>
+
+<script>
+import bus from '@/bus/bus'
+export default {
+  inject: ['name','app'],
+  data() {
+    return {
+      message: 'aaa'
+    }
+  },
+  created() {
+    let self = this
+    bus.$on('todo', (val) => {
+      console.log(val)
+      self.message = val
+    })
+  },
+}
+</script>
+
+<style>
+
+</style>
