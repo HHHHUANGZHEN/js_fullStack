534d1aa78bc49c0df43f61371600327d18a5c530
HuangZhen7 <HuangZhen_49@163.com>
2019-07-08 16:04:34 +0800

    react父子组件通信

---
+++ react/react-state-flow/src/List.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class List extends Component {
+  handleDelete = (e) => {
+    const { onDelete } = this.props
+    const id = parseInt(e.target.dataset.id)
+    console.log(id)
+    onDelete(id)
+    /**
+     * 
+     */
+  }
+  render() {
+    const { list } = this.props
+    return (
+      <>
+      <li>
+        name: {list.name}
+        age: {list.age}
+        school: {list.school}
+      </li>
+      <button onClick={this.handleDelete}>delete</button>
+      </>
+    )
+  }
+}
+
+export default List
